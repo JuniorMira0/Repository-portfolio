a5e90da797ef9bb118524449eece884b6d3dfc98
juniorMira <j-unyor14@hotmail.com>
2023-06-30 19:29:14 -0300

    first commit

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
+const header = document.querySelector('header');
 /* --------------- Grab elements from DOM --------------- */
 
 /* --------------- Sticky Navbar --------------- */
+function stickyNavBar() {
+  header.classList.toggle('scrolled', window.pageYOffset > 0);
+}
 
+stickyNavBar();
+
+window.addEventListener('scroll', stickyNavBar);
 /* --------------- Reveal Animation --------------- */
+
+let sr = ScrollReveal({
+  duration: 2500,
+  distance: '60px',
+});
+
+sr.reveal('.showcase-info', { delay: 600 });
+sr.reveal('.showcase-image', { origin: 'top', delay: 700 });
 
 /* --------------- Skills Progress Bar Animation --------------- */
 
